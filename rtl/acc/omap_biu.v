29a5862d5770b8296ee0143674fed51ad4ce23b7
heymesut <1299740655@qq.com>
2022-06-09 16:54:38 +0800

    <feat>[acc]: add omap_biu

--- rtl/acc/omap_biu.v
+++ rtl/acc/omap_biu.v
@@ -2,7 +2,7 @@
 // Project Name  : IC_Design
 // Author        : Heymesut
 // Created On    : 2022/06/04 20:00
-// Last Modified : 2022/06/05 15:05
+// Last Modified : 2022/06/09 16:53
 // File Name     : omap_biu.v
 // Description   : output feature map bus interface unit
 //
@@ -25,8 +25,7 @@ input  [7:0]       in_ch,
 input  [7:0]       out_ch,
 input  [15:0]      map_size,
 input  [31:0]      omap_base_addr,
-input  [7:0]       out_ch_cnt,
-input              omap_write_req,
+input              conv_start,
 
 // omap biu to arbiter req signal
 output             omap_biu2arb_req,
@@ -42,7 +41,69 @@ output             map_merger2omap_biu_rdy
 
 );
 
-assign omap_biu2arb_req = omap_write_req;
+// addr generation
+wire omap_biu_hdshk_in = map_merger2omap_biu_rdy & map_merger2omap_biu_vld;
+
+wire [31:0] omap_biu_addr_out = omap_biu_hdshk_in ?
+                              ((omap_biu2arb_addr==(20'd200703+omap_base_addr)) ?
+                                omap_base_addr : omap_biu2arb_addr+1) : omap_biu2arb_addr;
+
+// omap_biu output pipe stage
+wire omap_biu_vld_out  = omap_biu_hdshk_in;
+wire omap_biu_rdy_out;
+assign map_merger2omap_biu_rdy = omap_biu_rdy_out;
+
+sirv_gnrl_pipe_stage #(
+  .CUT_READY(0),
+  .DP(1),
+  .DW(32)
+) omap_biu2arb_data_pipe_stage(
+  .i_vld(omap_biu_vld_out),
+  .i_rdy(omap_biu_rdy_out),
+  .i_dat(map_merger2omap_biu_data),
+
+  .o_vld(omap_biu2arb_vld),
+  .o_rdy(omap_biu2arb_rdy),
+  .o_dat(omap_biu2arb_data),
+
+  .clk(clk),
+  .rst_n(rst_n)
+);
+
+sirv_gnrl_pipe_stage #(
+  .CUT_READY(0),
+  .DP(1),
+  .DW(32)
+) omap_biu2arb_addr_pipe_stage(
+  .i_vld(omap_biu_vld_out),
+  .i_rdy(omap_biu_rdy_out),
+  .i_dat(omap_biu_addr_out),
+
+  .o_vld(omap_biu2arb_vld),
+  .o_rdy(omap_biu2arb_rdy),
+  .o_dat(omap_biu2arb_addr),
+
+  .clk(clk),
+  .rst_n(rst_n)
+);
+
+
+// omap_biu2arb_req
+// when conv_start is triggered, set req; when the whole omap is transferred, reset req
+always @(posedge clk) begin
+  if(!rst_n)
+    omap_biu2arb_req <= 1'b0;
+  else
+    if(conv_start)
+      omap_biu2arb_req <= 1'b1;
+    else
+      if(omap_biu2arb_addr==(20'd200703+omap_base_addr))
+        omap_biu2arb_req <= 1'b0;
+      else
+        omap_biu2arb_req <= omap_biu2arb_req;
+end
+
+
 
 endmodule
 
